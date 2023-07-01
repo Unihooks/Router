63b7d27c4bbc68d2795dd96e47a6d1e4579f2fd8
syzonehq <syzone.hq@gmail.com>
2023-07-02 01:01:45 +0530

    blank router

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -3,61 +3,68 @@ pragma solidity ^0.8.19;
 
 
 import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {BalanceDelta} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
 
 
 contract Router {
+  error NotPoolManager();
+  error NotSelf();
+  error InvalidPool();
+  error LockFailure();
+  error HookNotImplemented();
+  
 
   function beforeInitialize(address, IPoolManager.PoolKey calldata, uint160) external virtual returns (bytes4) {
-        revert HookNotImplemented();
-    }
+    revert HookNotImplemented();
+  }
 
-    function afterInitialize(address, IPoolManager.PoolKey calldata, uint160, int24)
-      external
-      virtual
-      returns (bytes4)
-    {
-      revert HookNotImplemented();
-    }
+  function afterInitialize(address, IPoolManager.PoolKey calldata, uint160, int24)
+    external
+    virtual
+    returns (bytes4)
+  {
+    revert HookNotImplemented();
+  }
 
-    function beforeModifyPosition(address, IPoolManager.PoolKey calldata, IPoolManager.ModifyPositionParams calldata)
-      external
-      virtual
-      returns (bytes4)
-    {
-      revert HookNotImplemented();
-    }
+  function beforeModifyPosition(address, IPoolManager.PoolKey calldata, IPoolManager.ModifyPositionParams calldata)
+    external
+    virtual
+    returns (bytes4)
+  {
+    revert HookNotImplemented();
+  }
 
-    function afterModifyPosition(
-      address,
-      IPoolManager.PoolKey calldata,
-      IPoolManager.ModifyPositionParams calldata,
-      BalanceDelta
-    ) external virtual returns (bytes4) {
-      revert HookNotImplemented();
-    }
+  function afterModifyPosition(
+    address,
+    IPoolManager.PoolKey calldata,
+    IPoolManager.ModifyPositionParams calldata,
+    BalanceDelta
+  ) external virtual returns (bytes4) {
+    revert HookNotImplemented();
+  }
 
-    function beforeSwap(address, IPoolManager.PoolKey calldata, IPoolManager.SwapParams calldata)
-      external
-      virtual
-      returns (bytes4)
-    {
-      revert HookNotImplemented();
-    }
+  function beforeSwap(address, IPoolManager.PoolKey calldata, IPoolManager.SwapParams calldata)
+    external
+    virtual
+    returns (bytes4)
+  {
+    revert HookNotImplemented();
+  }
 
-    function afterSwap(address, IPoolManager.PoolKey calldata, IPoolManager.SwapParams calldata, BalanceDelta)
-      external
-      virtual
-      returns (bytes4)
-    {
-      revert HookNotImplemented();
-    }
+  function afterSwap(address, IPoolManager.PoolKey calldata, IPoolManager.SwapParams calldata, BalanceDelta)
+    external
+    virtual
+    returns (bytes4)
+  {
+    revert HookNotImplemented();
+  }
 
-    function beforeDonate(address, IPoolManager.PoolKey calldata, uint256, uint256) external virtual returns (bytes4) {
-      revert HookNotImplemented();
-    }
+  function beforeDonate(address, IPoolManager.PoolKey calldata, uint256, uint256) external virtual returns (bytes4) {
+    revert HookNotImplemented();
+  }
 
-    function afterDonate(address, IPoolManager.PoolKey calldata, uint256, uint256) external virtual returns (bytes4) {
-      revert HookNotImplemented();
-    }
+  function afterDonate(address, IPoolManager.PoolKey calldata, uint256, uint256) external virtual returns (bytes4) {
+    revert HookNotImplemented();
+  }
 
 }
